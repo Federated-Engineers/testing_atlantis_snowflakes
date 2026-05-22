90fffe222a083f2f86a1b5d35c91e5250441d41f
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-05-22 16:15:46 +0100

    added code owners

--- warehouse.tf
+++ warehouse.tf
@@ -2,5 +2,5 @@
 resource "snowflake_warehouse" "test_warehouse" {
   name                                = "ATLANTIS_TEST_WAREHOUSE"
   warehouse_type                      = "STANDARD"
-  warehouse_size                      = "X-SMALL"                           = "An ATLANTIS_TEST_WAREHOUSE."
+  warehouse_size                      = "X-SMALL"
 }

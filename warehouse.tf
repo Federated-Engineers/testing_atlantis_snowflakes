1616f7a42473e15e60a3884832b95146e3e135bb
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-05-22 16:11:52 +0100

    added code owners

--- warehouse.tf
+++ warehouse.tf
@@ -3,15 +3,9 @@ resource "snowflake_warehouse" "test_warehouse" {
   name                                = "ATLANTIS_TEST_WAREHOUSE"
   warehouse_type                      = "STANDARD"
   warehouse_size                      = "X-SMALL"
-  max_cluster_count                   = 2
-  min_cluster_count                   = 1
   scaling_policy                      = "ECONOMY"
   auto_suspend                        = 300
   auto_resume                         = true
   initially_suspended                 = false
   comment                             = "An ATLANTIS_TEST_WAREHOUSE."
-  enable_query_acceleration           = true
-  max_concurrency_level               = 3
-  statement_queued_timeout_in_seconds = 5
-  statement_timeout_in_seconds        = 10
 }

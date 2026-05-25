fa154d6b9786179f10751595691692f5c0425bb6
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-05-25 23:56:34 +0100

    testing

--- warehouse.tf
+++ warehouse.tf
@@ -1,6 +1,6 @@
-# Resource with all fields
-# resource "snowflake_warehouse" "test_warehouse" {
-#   name                                = "ATLANTIS_TEST_WAREHOUSE"
-#   warehouse_type                      = "STANDARD"
-#   warehouse_size                      = "X-SMALL"
-# }
+##Resource with all fields
+resource "snowflake_warehouse" "test_warehouse" {
+  name                                = "ATLANTIS_TEST_WAREHOUSE"
+  warehouse_type                      = "STANDARD"
+  warehouse_size                      = "X-SMALL"
+}
